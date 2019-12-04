722e4344ebb132b33247ab05ac4937777d157028
Anna Lubska <yaskrava@gmail.com>
2019-12-04 18:46:47 +0000

    initial commit v2

---
+++ backend/src/test/java/ua/com/parkhub/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.parkhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
