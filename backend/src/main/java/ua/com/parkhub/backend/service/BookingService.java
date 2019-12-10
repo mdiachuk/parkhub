0ab328aef163516036a9a39472cdd7c672def2b5
AnnaLub <yaskrava@gmail.com>
2019-12-10 17:46:09 +0200

    rename files

---
+++ backend/src/main/java/ua/com/parkhub/backend/service/BookingService.java
@@ -0,0 +1,18 @@
+package ua.com.parkhub.backend.service;
+
+import ua.com.parkhub.backend.model.Booking;
+import ua.com.parkhub.backend.model.Parking;
+import ua.com.parkhub.backend.service.factory.Factory;
+
+public class BookingService {
+
+    Parking findParkingById(Long id) {
+        return Factory.createParking(80);
+    }
+
+    Booking createBooking(Long customerId, Long slotId) {
+        return Factory.createBooking(customerId, slotId);
+    }
+
+
+}
