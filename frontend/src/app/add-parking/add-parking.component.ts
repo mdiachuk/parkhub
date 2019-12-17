147d212f154bcfd1307a4226da797217beadbd42
Aliona Romanenko <alionka.romanenko@gmail.com>
2019-12-17 11:35:29 +0200

    base front made

--- frontend/src/app/add-parking/add-parking.component.ts
+++ frontend/src/app/add-parking/add-parking.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
+
+export interface Food {
+  value: string;
+  viewValue: string;
+}
+
 
 @Component({
   selector: 'app-add-parking',
@@ -8,53 +14,46 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./add-parking.component.scss']
 })
 export class AddParkingComponent {
+  
   formGroup: FormGroup;
   titleAlert: string = 'This field is required';
   post: any = '';
-
+  CityArray: any = ['Kyiv', 'Lviv', 'Chernivtsi', 'Dnipro', 'Kharkiv'];
+  nameregex: RegExp = /^[a-zA-Z ]*$/
+  onlyPositiveIntegersregex: RegExp = /^[1-9]+[0-9]*$/  
+  streetregex: RegExp = /^([a-zA-Z -]+)$/
+  buildingregex: RegExp = /^([a-zA-Z0-9 -]+)$/
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.createForm();
-    this.setChangeValidate()
   }
 
   createForm() {
-    let emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    // let nameregex: RegExp = /^[a-zA-Z\s]*$/
+    // let onlyPositiveIntegersregex: RegExp = /^[1-9]+[0-9]*$/  
+    // let streetregex: RegExp = /^([a-zA-Z -]+)$/
+    // let buildingregex: RegExp = /^\w-$/ //not sure if correct(ask)
     this.formGroup = this.formBuilder.group({
-      'email': [null, [Validators.required, Validators.pattern(emailregex)], this.checkInUseEmail],
-      'name': [null, Validators.required],
-      'password': [null, [Validators.required, this.checkPassword]],
-      'description': [null, [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
-      'validate': ''
+      'parkingName': [null, [Validators.required, Validators.pattern(this.nameregex)]],
+      'slotsNumber': [null, [Validators.required, Validators.pattern(this.onlyPositiveIntegersregex)]],
+      'tariff': [null, [Validators.required, Validators.pattern(this.onlyPositiveIntegersregex)]],
+      'city': [null],
+      'street': [null,[Validators.required, Validators.pattern(this.streetregex)]],
+      'building': [null,[Validators.required, Validators.pattern(this.buildingregex)]]
+     // 'description': [null, [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+     // 'validate': ''
     });
   }
 
-  setChangeValidate() {
-    this.formGroup.get('validate').valueChanges.subscribe(
-      (validate) => {
-        if (validate == '1') {
-          this.formGroup.get('name').setValidators([Validators.required, Validators.minLength(3)]);
-          this.titleAlert = "You need to specify at least 3 characters";
-        } else {
-          this.formGroup.get('name').setValidators(Validators.required);
-        }
-        this.formGroup.get('name').updateValueAndValidity();
-      }
-    )
-  }
 
-  get name() {
-    return this.formGroup.get('name') as FormControl
-  }
+  // checkPassword(control) {
+  //   let enteredPassword = control.value
+  //   let passwordCheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+  //   return (!passwordCheck.test(enteredPassword) && enteredPassword) ? { 'requirements': true } : null;
+  // }
 
-  checkPassword(control) {
-    let enteredPassword = control.value
-    let passwordCheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
-    return (!passwordCheck.test(enteredPassword) && enteredPassword) ? { 'requirements': true } : null;
-  }
-
-  checkInUseEmail(control) {
+  checkInUseParkingName(control) {
     // mimic http database access
     let db = ['tony@gmail.com'];
     return new Observable(observer => {
@@ -66,15 +65,32 @@ export class AddParkingComponent {
     })
   }
 
-  getErrorEmail() {
-    return this.formGroup.get('email').hasError('required') ? 'Field is required' :
-      this.formGroup.get('email').hasError('pattern') ? 'Not a valid emailaddress' :
-        this.formGroup.get('email').hasError('alreadyInUse') ? 'This emailaddress is already in use' : '';
+  getErrorParkingName() {
+    return this.formGroup.get('parkingName').hasError('required') ? 'Field is required' :
+      this.formGroup.get('parkingName').hasError('pattern') ? 'Not a valid parking name': '';
+       // this.formGroup.get('parkingName').hasError('alreadyInUse') ? 'This emailaddress is already in use' : '';
   }
 
-  getErrorPassword() {
-    return this.formGroup.get('password').hasError('required') ? 'Field is required (at least eight characters, one uppercase letter and one number)' :
-      this.formGroup.get('password').hasError('requirements') ? 'Password needs to be at least eight characters, one uppercase letter and one number' : '';
+  getErrorSlotsNumber() {
+    return this.formGroup.get('slotsNumber').hasError('required') ? 'Field is required' :
+      this.formGroup.get('slotsNumber').hasError('pattern') ? 'Not a valid number of slots.Should be positive integer': '';
+       
+  }
+
+  getErrorTariff() {
+    return this.formGroup.get('tariff').hasError('required') ? 'Field is required' :
+      this.formGroup.get('tariff').hasError('pattern') ? 'Not a valid tariff.Should be positive integer': '';
+       
+  }
+
+  getErrorStreet() {
+    return this.formGroup.get('street').hasError('required') ? 'Field is required' :
+      this.formGroup.get('street').hasError('pattern') ? 'Not a valid name of street.': '';
+       
+  }
+  getErrorBuilding() {
+    return this.formGroup.get('building').hasError('required') ? 'Field is required ' :
+      this.formGroup.get('building').hasError('pattern') ? 'Not a valid building' : '';
   }
 
   onSubmit(post) {
