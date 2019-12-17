95e9e4330311c6e3301680029feb3414ace1a648
Alex Syrin <kpaxianukr@gmail.com>
2019-12-17 23:02:38 +0200

    V5-Inital

--- backend/src/main/java/ua/com/parkhub/controller/AdminController.java
+++ backend/src/main/java/ua/com/parkhub/controller/AdminController.java
@@ -1,7 +1,39 @@
 package ua.com.parkhub.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import ua.com.parkhub.persistence.entities.User;
+import ua.com.parkhub.persistence.entities.UserRole;
+import ua.com.parkhub.service.AdminService;
+
+@RestController @CrossOrigin(origins = "http://localhost:4200")
 public class AdminController {
+    public AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping("/admin/{id}")
+    public User getUserById(@PathVariable("id") long id){
+        return adminService.findUserById(id);
+    }
+
+    @PostMapping("/admin")
+    public void setUserRole(long id, UserRole role){
+        adminService.setRole(id, role);
+    }
+
+    /*@GetMapping("/test")
+    public TestDTO test() {
+//        User user = adminService.findUserById(id);
+        TestDTO test = new TestDTO();
+        test.setName("Vasya");
+//        test.setName(user.getFirstName());
+//        return test;
+        return test;
+    }*/
 }
--- backend/src/main/java/ua/com/parkhub/service/AdminService.java
+++ backend/src/main/java/ua/com/parkhub/service/AdminService.java
@@ -1,4 +1,25 @@
 package ua.com.parkhub.service;
+import org.springframework.stereotype.Service;
+import ua.com.parkhub.persistence.entities.User;
+import ua.com.parkhub.persistence.entities.UserRole;
+import ua.com.parkhub.persistence.impl.UserDAO;
 
-public class AdminService {
+@Service
+public class AdminService  {
+    public UserDAO userDAO;
+
+    public AdminService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public User findUserById(long id){
+        return userDAO.findElementById(id);
+    }
+
+    public void setRole(Long id, UserRole role){
+        User targetUser = userDAO.findElementById(id);
+        targetUser.setRole(role);
+        userDAO.updateElement(targetUser);
+    }
 }
+
