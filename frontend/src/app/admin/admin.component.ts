2a9c2d25ca6835fac443171534ed4b7924289da1
Alex Syrin <kpaxianukr@gmail.com>
2019-12-18 11:43:51 +0200

    V5-Angular-update

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../service/admin.service'
+import { Admin } from '../Classes/admin'
+import { ActivatedRoute, Router} from '@angular/router'
+import { HttpClient } from '@angular/common/http'
+
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  admin = new Admin()
+  
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private adminService: AdminService,
+    private httpClient: HttpClient,
+  ) { }
 
   ngOnInit() {
+    this.try();
+    // this.adminService.getUserById().subscribe(response => this.admin = response);
+  }
+  try(){
+    this.httpClient.get<Admin>("http://localhost:8080/admin/1").subscribe(response => this.admin = response);
   }
 
 }
--- frontend/src/app/service/admin.service.ts
+++ frontend/src/app/service/admin.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { Admin } from '../Classes/admin'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUserById(id: number): Observable<Admin>{
+    return this.http.get<Admin>('//localhost:8080/admin/{id}');
+  }
 }
