ebf1c1ba250c62a5902f30346c0b5cfa1d61df9d
Max Dyachuk <dyachukmv1@gmail.com>
2019-12-30 16:53:02 +0200

    Change liquibase script for creating uuid_token and uuid_token_type tables

--- backend/src/main/resources/db/changelog/sqlFile/sql_script_for_db_update_v5.sql
+++ backend/src/main/resources/db/changelog/sqlFile/sql_script_for_db_update_v5.sql
@@ -1,8 +1,13 @@
-CREATE TABLE IF NOT EXISTS "park_hub".uuid_token (
+CREATE TABLE IF NOT EXISTS "park_hub".uuid_token_type (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    type VARCHAR NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT 'true'
+);
 
+CREATE TABLE IF NOT EXISTS "park_hub".uuid_token (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	user_id BIGINT NOT NULL REFERENCES park_hub.user(id),
 	token VARCHAR(36) NOT NULL,
-	expiration_date TIMESTAMP WITH TIME ZONE NOT NULL
-
+	expiration_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    token_type_id BIGINT NOT NULL REFERENCES park_hub.uuid_token_type(id)
 );
