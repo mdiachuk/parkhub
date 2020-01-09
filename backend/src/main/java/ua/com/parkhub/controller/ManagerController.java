81119e8b59feeeb0ee879de0dd3b7f6a215e7993
amsterdamnnn <yaroslav.podzirei@gmail.com>
2020-01-09 21:45:38 +0200

    added preAuthorize

--- backend/src/main/java/ua/com/parkhub/controller/ManagerController.java
+++ backend/src/main/java/ua/com/parkhub/controller/ManagerController.java
@@ -2,6 +2,7 @@ package ua.com.parkhub.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ua.com.parkhub.dto.ParkingDTO;
 import ua.com.parkhub.dto.ParkingUpdateRequestDTO;
@@ -28,18 +29,21 @@ public class ManagerController {
         this.parkingRequestDtoToModelMapper = parkingRequestDtoToModelMapper;
     }
 
+    @PreAuthorize("hasRole('MANAGER') ")
     @GetMapping()
     public ResponseEntity<List<ParkingDTO>> getAllParking() {
 
         return ResponseEntity.ok(parkingService.findAllParking().stream().map(parkingMapper::transform).collect(Collectors.toList()));
     }
 
+    @PreAuthorize("hasRole('MANAGER') ")
     @GetMapping(value = "/{parkingId}")
     public ResponseEntity<ParkingDTO> getParkingById(@PathVariable("parkingId") Long parkingId){
 
         return ResponseEntity.ok(parkingMapper.transform(parkingService.findParkingById(parkingId)));
     }
 
+    @PreAuthorize("hasRole('MANAGER') ")
     @PutMapping(value = "/{parkingId}")
     public ResponseEntity<Void> updateParking(@PathVariable("parkingId") Long parkingId, @RequestBody ParkingUpdateRequestDTO requestDTO) {
         parkingService.updateParking(parkingId, parkingRequestDtoToModelMapper.transform(requestDTO));
