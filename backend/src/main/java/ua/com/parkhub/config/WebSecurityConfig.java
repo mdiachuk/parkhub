96d92783994279370f5792b734ad74fe08fd00d4
Valerie <valeriesalivon@gmail.com>
2020-01-09 20:41:32 +0200

    Deprecated method configure removed from WebSecurityConfig

--- backend/src/main/java/ua/com/parkhub/config/WebSecurityConfig.java
+++ backend/src/main/java/ua/com/parkhub/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package ua.com.parkhub.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,9 +24,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .antMatchers("/api/login");
     }
 
-    @Autowired
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
