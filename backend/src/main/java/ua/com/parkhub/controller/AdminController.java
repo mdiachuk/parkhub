955ce1a4169617630a2eaea4ef353e8c8eed696e
Valerie <valeriesalivon@gmail.com>
2020-01-10 17:14:22 +0200

    Security for admin service added

--- backend/src/main/java/ua/com/parkhub/controller/AdminController.java
+++ backend/src/main/java/ua/com/parkhub/controller/AdminController.java
@@ -2,6 +2,7 @@ package ua.com.parkhub.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ua.com.parkhub.dto.AdminDTO;
 import ua.com.parkhub.dto.AdminSupportTicketDTO;
@@ -20,6 +21,7 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/api/admin/{id}")
     public AdminDTO getUserByID(@PathVariable("id")long id ){
         AdminDTO targetUserDTO = new AdminDTO();
@@ -29,22 +31,26 @@ public class AdminController {
         return targetUserDTO;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/api/admin/{id}")
     public ResponseEntity setRole(@RequestBody AdminDTO adminDTO){
         adminService.setRole(adminDTO.getId());
         return ResponseEntity.ok().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/api/admin/ticketlist")
     public List<AdminSupportTicketDTO> getTicketList(){
         return adminService.getTicketsList();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/api/admin/ticket/{id}")
     public AdminSupportTicketDTO getTicketById(@PathVariable("id")long id){
         return adminService.getSingleTicketById(id);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/api/admin/ticketlistcounter")
     public AdminTicketCounterDTO getTicketCounter(){
         return adminService.getTicketCounter();
