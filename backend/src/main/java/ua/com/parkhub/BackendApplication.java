8ea2a0687ec1de00367efc492243600c678d05c3
Dell <yaskrava@gmail.com>
2020-01-13 16:45:56 +0200

    frontend improvements

--- backend/src/main/java/ua/com/parkhub/BackendApplication.java
+++ backend/src/main/java/ua/com/parkhub/BackendApplication.java
@@ -12,14 +12,6 @@ import java.util.List;
 @SpringBootApplication
 
 public class BackendApplication {
-    @Bean
-    CommandLineRunner init(BookingService bookingService) {
-        return args -> {
-            List<Slot> list = bookingService.findAllAvailableSlots(1578907342000L, 1578936600000L);
-            System.out.println(list);
-        };
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
