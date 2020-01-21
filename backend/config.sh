a1ab1ed7ad46b1d7248e0f494b0b9eeba5cfa1f4
Anna Lubska <yaskrava@gmail.com>
2020-01-21 23:58:34 +0200

    fix for independent CI and manual deploy

--- backend/config.sh
+++ backend/config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Generating JDBC Properties file"
-sed -i -e "s;%GC_INSTANCE%;$1;g" src/main/webapp/WEB-INF/appengine-web.xml
-sed -i -e "s;%GC_DB_NAME%;$2;g" src/main/webapp/WEB-INF/appengine-web.xml
-sed -i -e "s;%DB_USER%;$3;g" src/main/webapp/WEB-INF/appengine-web.xml
-sed -i -e "s;%DB_PASS%;$4;g" src/main/webapp/WEB-INF/appengine-web.xml
+sed -e "s;%GC_INSTANCE%;$1;g" -e "s;%GC_DB_NAME%;$2;g" \
+    -e "s;%DB_USER%;$3;g" -e "s;%DB_PASS%;$4;g" \
+    src/main/webapp/WEB-INF/appengine-web.xml.template > src/main/webapp/WEB-INF/appengine-web.xml
