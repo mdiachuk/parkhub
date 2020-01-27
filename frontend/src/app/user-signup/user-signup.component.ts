a6c28c72eb1c13d6bdc736d41b9fe59eb3c30bb2
Max Dyachuk <dyachukmv1@gmail.com>
2020-01-27 19:23:45 +0000

    Fix frontend

--- frontend/src/app/user-signup/user-signup.component.ts
+++ frontend/src/app/user-signup/user-signup.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../model/user';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { SignUpService } from '../service/signup.service';
-import { MatSnackBar } from '@angular/material';
 import { HttpClient } from '@angular/common/http';
 import { ConfirmPasswordValidator } from '../validation/confirm-password.validator';
+import { AddParkingDialogComponent } from '../add-parking-dialog/add-parking-dialog.component';
+import { MatDialogRef, MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-user-signup',
@@ -17,9 +18,10 @@ export class UserSignupComponent implements OnInit {
   user: User;
   message: string;
   isCreated: boolean;
+  loading: boolean;
 
   constructor(private signUpService: SignUpService, private fb: FormBuilder,
-    private http: HttpClient, private snackBar: MatSnackBar) { }
+    private http: HttpClient, private dialog: MatDialog) { }
 
   ngOnInit() {
     this.signupForm = this.fb.group({
@@ -31,21 +33,24 @@ export class UserSignupComponent implements OnInit {
       confirmPassword: ['']
     }, { validator: ConfirmPasswordValidator.matchPassword });
     this.isCreated = false;
+    this.loading = false;
   }
 
   register(): void {
+    this.loading = true;
     this.user = new User(this.signupForm.get('firstName').value,
       this.signupForm.get('lastName').value,
       this.signupForm.get('email').value,
       this.signupForm.get('phoneNumber').value,
       this.signupForm.get('password').value,
       this.signupForm.get('confirmPassword').value);
-
     this.signUpService.registerUser(this.user).subscribe(response => {
       this.isCreated = true;
+      this.loading = false;
     }, err => {
       this.message = err.error;
-      this.openSnackBar(this.message);
+      this.openDialog(this.message);
+      this.loading = false;
     });
   }
 
@@ -55,10 +60,10 @@ export class UserSignupComponent implements OnInit {
     this.signupForm.reset();
   }
 
-  openSnackBar(message: string) {
-    this.snackBar.open(message, 'Close', {
-      duration: 4000,
+  openDialog( message: string): MatDialogRef<AddParkingDialogComponent> {
+    return this.dialog.open(AddParkingDialogComponent, {
+      width: '350px',
+      data: {message}
     });
   }
-
 }
